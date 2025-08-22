b74022753a8d1894f2652a59868094117be809cf
benjag22 <benjagonzalez2022@udec.cl>
2025-08-22 03:07:22 -0400

    feat: add the parent entity representing the condition of a part of the car

---
+++ src/main/java/com/mechanical_workshop_usm/mechanical_condition/persistence/model/MechanicalCondition.java
@@ -0,0 +1,28 @@
+package com.mechanical_workshop_usm.mechanical_condition.persistence.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "mechanical_condition")
+@Inheritance(strategy = InheritanceType.JOINED)
+@Getter
+@Setter
+public class MechanicalCondition {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "part_name", nullable = false, length = 64)
+    private String partName;
+
+    @Column(name = "part_condition_state", length = 128)
+    private String partConditionState;
+
+    public MechanicalCondition() {}
+
+    public MechanicalCondition(String partName, String partConditionState) {
+        this.partName = partName;
+        this.partConditionState = partConditionState;
+    }
+}
