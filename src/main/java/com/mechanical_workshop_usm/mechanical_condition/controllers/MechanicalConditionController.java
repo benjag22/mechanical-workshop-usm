89e09a4401dfcc8988ec995162ed08137507e720
benjag22 <benjagonzalez2022@udec.cl>
2025-08-22 03:10:11 -0400

    fix: solve unnecesary bracket

--- src/main/java/com/mechanical_workshop_usm/mechanical_condition/controllers/MechanicalConditionController.java
+++ src/main/java/com/mechanical_workshop_usm/mechanical_condition/controllers/MechanicalConditionController.java
@@ -1,7 +1,7 @@
 package com.mechanical_workshop_usm.mechanical_condition.controllers;
 
 import com.mechanical_workshop_usm.mechanical_condition.persistence.model.InteriorCondition;
-import com.mechanical_workshop_usm.mechanical_condition.persistence.model.MechanicalCondition;}
+import com.mechanical_workshop_usm.mechanical_condition.persistence.model.MechanicalCondition;
 import com.mechanical_workshop_usm.mechanical_condition.persistence.repository.MechanicalConditionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
