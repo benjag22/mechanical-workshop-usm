a04fe451e978587ed25772751f7244f99babece1
benjag22 <benjagonzalez2022@udec.cl>
2025-08-23 01:42:24 -0400

    feat: create work order model

---
+++ src/main/java/com/mechanical_workshop_usm/work_order_realized_service_module/persistence/model/WorkOrder.java
@@ -0,0 +1,31 @@
+package com.mechanical_workshop_usm.work_order_realized_service_module.persistence.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalTime;
+import java.util.Date;
+
+@Entity
+@Table(name = "work_order")
+public class WorkOrder {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "estimated_date", nullable = false)
+    @Getter
+    @Setter
+    private Date estimatedDate;
+
+    @Column(name = "estimated_time", nullable = false)
+    @Getter
+    @Setter
+    private LocalTime estimatedTime;
+
+    public WorkOrder() {}
+
+    public WorkOrder(Date estimatedDate, LocalTime estimatedTime) {}
+}
