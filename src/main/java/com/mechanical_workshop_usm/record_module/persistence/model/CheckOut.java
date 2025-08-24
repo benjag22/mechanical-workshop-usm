ba5f5a8cee0d9fe67fc777afa38b0b35738047c8
benjag22 <benjagonzalez2022@udec.cl>
2025-08-24 00:07:47 -0400

    feat: create check out child class model

---
+++ src/main/java/com/mechanical_workshop_usm/record_module/persistence/model/CheckOut.java
@@ -0,0 +1,29 @@
+package com.mechanical_workshop_usm.record_module.persistence.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.PrimaryKeyJoinColumn;
+import jakarta.persistence.Table;
+
+import java.time.LocalTime;
+import java.util.Date;
+
+@Entity
+@Table(name = "out_state")
+@PrimaryKeyJoinColumn(name = "record_state_id")
+public class CheckOut extends RecordState {
+
+    @Column(name = "vehicle_diagnosis")
+    private String vehicleDiagnosis;
+
+    @Column(name = "rating")
+    private byte rating;
+
+    public CheckOut() {
+        super();
+    }
+
+    public CheckOut(String vehicleDiagnosis, byte rating, Date entryDate, LocalTime entryTime, int mileage) {
+        super(entryDate, entryTime, mileage);
+    }
+}
