fe54b6a1fc9e173a9014a0c3624718894ed4b035
benjag22 <benjagonzalez2022@udec.cl>
2025-08-26 20:32:01 -0400

    feat: create client info service with createClient method

---
+++ src/main/java/com/mechanical_workshop_usm/client_info_module/ClientInfoService.java
@@ -0,0 +1,31 @@
+package com.mechanical_workshop_usm.client_info_module;
+
+import com.mechanical_workshop_usm.client_info_module.dtos.CreateClientRequest;
+import com.mechanical_workshop_usm.client_info_module.dtos.CreateClientResponse;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClientInfoService {
+
+    private final ClientInfoRepository clientInfoRepository;
+    private final ClientInfoValidator clientInfoValidator;
+
+    public ClientInfoService(ClientInfoRepository clientInfoRepository, ClientInfoValidator clientInfoValidator) {
+        this.clientInfoRepository = clientInfoRepository;
+        this.clientInfoValidator = clientInfoValidator;
+    }
+
+    public CreateClientResponse createClient(CreateClientRequest createClientRequest) {
+        clientInfoValidator.validateOnCreate(createClientRequest);
+        ClientInfo clientInfo = new ClientInfo(
+                createClientRequest.firstName(),
+                createClientRequest.lastName(),
+                createClientRequest.emailAddress(),
+                createClientRequest.address(),
+                createClientRequest.cellphoneNumber()
+        );
+        ClientInfo savedClientInfo = clientInfoRepository.save(clientInfo);
+        return new CreateClientResponse(savedClientInfo.getId(), savedClientInfo.getCellphoneNumber());
+    }
+
+}
