733a044958f0e73e7dd7eb8e026c76989ba704b3
ak0mak0 <akmak2470@gmail.com>
2025-09-08 02:48:54 -0300

    CreateMechanicRequest Test

---
+++ src/test/java/com/mechanical_workshop_usm/mechanic_info_module/dtos/CreateMechanicResponseTest.java
@@ -0,0 +1,15 @@
+package com.mechanical_workshop_usm.mechanic_info_module.dtos;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CreateMechanicResponseTest {
+
+    @Test
+    void testRecordFields() {
+        CreateMechanicResponse dto = new CreateMechanicResponse(42L, "MECH2025");
+
+        assertEquals(42L, dto.id());
+        assertEquals("MECH2025", dto.registrationNumber());
+    }
+}
