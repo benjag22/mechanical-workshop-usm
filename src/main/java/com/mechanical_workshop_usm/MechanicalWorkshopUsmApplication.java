998c64131145a6abba642051032dced22bfc2ced
benjag22 <benjag.gonzalez13@gmail.com>
2025-11-09 21:04:16 -0300

    !feat: remove env set properties

--- src/main/java/com/mechanical_workshop_usm/MechanicalWorkshopUsmApplication.java
+++ src/main/java/com/mechanical_workshop_usm/MechanicalWorkshopUsmApplication.java
@@ -1,19 +1,12 @@
 package com.mechanical_workshop_usm;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.Objects;
 
 @SpringBootApplication
 public class MechanicalWorkshopUsmApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-		System.setProperty("DB_URL", Objects.requireNonNull(dotenv.get("DB_URL")));
-		System.setProperty("DB_USERNAME", Objects.requireNonNull(dotenv.get("DB_USERNAME")));
-		System.setProperty("DB_PASSWORD", Objects.requireNonNull(dotenv.get("DB_PASSWORD")));
 		SpringApplication.run(MechanicalWorkshopUsmApplication.class, args);
 	}
 
