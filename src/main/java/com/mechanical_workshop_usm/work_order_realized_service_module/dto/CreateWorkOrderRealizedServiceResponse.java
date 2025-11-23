d28df94a3a99d89e5ccbe0c70ae9fba8f566f21c
benjag22 <benjag.gonzalez13@gmail.com>
2025-11-23 13:17:40 -0300

    feat: add required properties

--- src/main/java/com/mechanical_workshop_usm/work_order_realized_service_module/dto/CreateWorkOrderRealizedServiceResponse.java
+++ src/main/java/com/mechanical_workshop_usm/work_order_realized_service_module/dto/CreateWorkOrderRealizedServiceResponse.java
@@ -4,15 +4,15 @@ package com.mechanical_workshop_usm.work_order_realized_service_module.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record CreateWorkOrderRealizedServiceResponse(
-        @Schema(description = "ID del registro")
+        @Schema(requiredMode = Schema.RequiredMode.REQUIRED, description = "ID del registro")
         int id,
 
-        @Schema(description = "ID del work order asociado")
+        @Schema(requiredMode = Schema.RequiredMode.REQUIRED, description = "ID del work order asociado")
         int workOrderId,
 
-        @Schema(description = "ID del work service asociado")
+        @Schema(requiredMode = Schema.RequiredMode.REQUIRED, description = "ID del work service asociado")
         int workServiceId,
 
-        @Schema(description = "Si el servicio está finalizado")
+        @Schema(requiredMode = Schema.RequiredMode.REQUIRED, description = "Si el servicio está finalizado")
         boolean finalized
 ) {}
